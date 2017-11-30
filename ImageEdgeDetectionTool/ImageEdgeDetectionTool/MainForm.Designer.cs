625fbf8c4533e71aaa398b4f114a23316394e56a
Mikko Lerto <mikko.lerto@gmail.com>
2017-11-30 22:15:30 +0100

    made somethings working

--- ImageEdgeDetectionTool/ImageEdgeDetectionTool/MainForm.Designer.cs
+++ ImageEdgeDetectionTool/ImageEdgeDetectionTool/MainForm.Designer.cs
@@ -30,18 +30,19 @@
         {
             this.loadImage = new System.Windows.Forms.Button();
             this.saveImage = new System.Windows.Forms.Button();
-            this.Image = new System.Windows.Forms.PictureBox();
+            this.ImagePreview = new System.Windows.Forms.PictureBox();
             this.EdgeDetectionList = new System.Windows.Forms.ListBox();
-            ((System.ComponentModel.ISupportInitialize)(this.Image)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ImagePreview)).BeginInit();
             this.SuspendLayout();
             // 
             // loadImage
             // 
             this.loadImage.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.loadImage.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.loadImage.Location = new System.Drawing.Point(575, 8);
+            this.loadImage.Location = new System.Drawing.Point(1054, 15);
+            this.loadImage.Margin = new System.Windows.Forms.Padding(6);
             this.loadImage.Name = "loadImage";
-            this.loadImage.Size = new System.Drawing.Size(148, 57);
+            this.loadImage.Size = new System.Drawing.Size(271, 105);
             this.loadImage.TabIndex = 0;
             this.loadImage.Text = "Load Image";
             this.loadImage.UseVisualStyleBackColor = true;
@@ -51,46 +52,52 @@
             // 
             this.saveImage.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.saveImage.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.saveImage.Location = new System.Drawing.Point(575, 365);
+            this.saveImage.Location = new System.Drawing.Point(1054, 674);
+            this.saveImage.Margin = new System.Windows.Forms.Padding(6);
             this.saveImage.Name = "saveImage";
-            this.saveImage.Size = new System.Drawing.Size(148, 57);
+            this.saveImage.Size = new System.Drawing.Size(271, 105);
             this.saveImage.TabIndex = 1;
             this.saveImage.Text = "Save Image";
             this.saveImage.UseVisualStyleBackColor = true;
             this.saveImage.Click += new System.EventHandler(this.saveImage_Click);
             // 
-            // Image
+            // ImagePreview
             // 
-            this.Image.Location = new System.Drawing.Point(15, 8);
-            this.Image.Name = "Image";
-            this.Image.Size = new System.Drawing.Size(552, 413);
-            this.Image.TabIndex = 2;
-            this.Image.TabStop = false;
+            this.ImagePreview.Location = new System.Drawing.Point(28, 15);
+            this.ImagePreview.Margin = new System.Windows.Forms.Padding(6);
+            this.ImagePreview.Name = "ImagePreview";
+            this.ImagePreview.Size = new System.Drawing.Size(1012, 762);
+            this.ImagePreview.TabIndex = 2;
+            this.ImagePreview.TabStop = false;
             // 
             // EdgeDetectionList
             // 
             this.EdgeDetectionList.FormattingEnabled = true;
+            this.EdgeDetectionList.ItemHeight = 24;
             this.EdgeDetectionList.Items.AddRange(new object[] {
+            "-Original-",
             "Zen filter",
             "Some filter"});
-            this.EdgeDetectionList.Location = new System.Drawing.Point(575, 96);
+            this.EdgeDetectionList.Location = new System.Drawing.Point(1054, 177);
+            this.EdgeDetectionList.Margin = new System.Windows.Forms.Padding(6);
             this.EdgeDetectionList.Name = "EdgeDetectionList";
-            this.EdgeDetectionList.Size = new System.Drawing.Size(148, 238);
+            this.EdgeDetectionList.Size = new System.Drawing.Size(268, 436);
             this.EdgeDetectionList.TabIndex = 4;
             this.EdgeDetectionList.SelectedIndexChanged += new System.EventHandler(this.EdgeDetectionList_SelectedIndexChanged);
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(731, 432);
+            this.ClientSize = new System.Drawing.Size(1340, 798);
             this.Controls.Add(this.EdgeDetectionList);
-            this.Controls.Add(this.Image);
+            this.Controls.Add(this.ImagePreview);
             this.Controls.Add(this.saveImage);
             this.Controls.Add(this.loadImage);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "MainForm";
             this.Text = "Image Edge Detection";
-            ((System.ComponentModel.ISupportInitialize)(this.Image)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ImagePreview)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -99,7 +106,7 @@
 
         private System.Windows.Forms.Button loadImage;
         private System.Windows.Forms.Button saveImage;
-        private System.Windows.Forms.PictureBox Image;
+        private System.Windows.Forms.PictureBox ImagePreview;
         private System.Windows.Forms.ListBox EdgeDetectionList;
     }
 }
--- ImageEdgeDetectionTool/ImageEdgeDetectionTool/MainForm.cs
+++ ImageEdgeDetectionTool/ImageEdgeDetectionTool/MainForm.cs
@@ -13,12 +13,16 @@ namespace ImageEdgeDetectionTool
     public partial class MainForm : Form
     {
         private Bitmap originalBitmap = null;
-        private Bitmap selectedBitmap = null;
+        private Bitmap previewBitmap = null;
         private Bitmap resultBitmap = null;
 
         public MainForm()
         {
             InitializeComponent();
+            
+            //just some proof of concept stuff
+            originalBitmap = previewBitmap = new Bitmap(ImageEdgeDetectionTool.Properties.Resources.panda);
+            ImagePreview.Image = originalBitmap;
         }
 
         private void loadImage_Click(object sender, EventArgs e)
@@ -32,22 +36,33 @@ namespace ImageEdgeDetectionTool
         }
 
         private void EdgeDetectionList_SelectedIndexChanged(object sender, EventArgs e)
-        {
+        {   
+            //initialize the preview
+            previewBitmap = originalBitmap;
+
             string selected = EdgeDetectionList.SelectedItem.ToString();
             switch (selected)
             {
             case "-Original-":
                 {
-                    resultBitmap = originalBitmap;
+                    previewBitmap = originalBitmap;
+                    System.Diagnostics.Debug.WriteLine("111111111111111111111111111111111111111111");
                     break;
                 }
             case "Zen filter":
                 {
-                    //bitmapResult = s.Laplacian3x3Filter(false);
+                        //previewBitmap = previewBitmap.Laplacian3x3Filter(true);
+
+                        //just some proof of concept stuff
+                        previewBitmap = new Bitmap(ImageEdgeDetectionTool.Properties.Resources.tiger);
+                    System.Diagnostics.Debug.WriteLine("222222222222222222222222222222222");
                     break;
                 }
             case "Some filter":
                 {
+                        //just some proof of concept stuff
+                        previewBitmap = new Bitmap(ImageEdgeDetectionTool.Properties.Resources.hippo);
+                        System.Diagnostics.Debug.WriteLine("333333333333333333333333333333333");
                     //bitmapResult = selectedSource.Laplacian3x3Filter(true);
                     break;
                 }
@@ -56,6 +71,8 @@ namespace ImageEdgeDetectionTool
                     break;
                 }
             }
+            //send the image into the preview
+            ImagePreview.Image = previewBitmap;
         }
 
     }
