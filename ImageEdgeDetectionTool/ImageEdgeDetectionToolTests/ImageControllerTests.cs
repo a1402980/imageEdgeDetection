fa96823f692d3061de620e64e8858390b9e45c06
Ibrahim Beqiri <ibraa.cs@msn.com>
2017-12-07 20:41:40 +0100

    tests

---
+++ ImageEdgeDetectionTool/ImageEdgeDetectionToolTests/ImageControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ImageEdgeDetectionToolTests
+{
+    [TestClass]
+    public class ImageControllerTests
+    {
+        [TestMethod]
+        public void NightFilterTest()
+        {
+        }
+        [TestMethod]
+        public void ZenFilterTest()
+        {
+
+        }
+        [TestMethod]
+        public void InputFileTest()
+        {
+
+        }
+        [TestMethod]
+        public void OutputFileTest()
+        {
+
+        }
+        [TestMethod]
+        public void InputFileExceptionTest()
+        {
+
+        }
+    }
+}
---
+++ ImageEdgeDetectionTool/ImageEdgeDetectionToolTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ImageEdgeDetectionToolTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ImageEdgeDetectionToolTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f8360652-96eb-4786-9c40-ae3e88710c2b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
