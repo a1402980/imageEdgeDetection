166082f36a024ef3c45b5e1678fd243c0633763b
Mikko Lerto <mikko.lerto@gmail.com>
2017-12-07 15:01:20 +0100

    interface addition

--- ImageEdgeDetectionTool/ImageEdgeDetectionTool/IFilters.cs
+++ ImageEdgeDetectionTool/ImageEdgeDetectionTool/IFilters.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ImageEdgeDetectionTool
 {
-    interface IFilters
+    public interface IFilters
     {
+        Bitmap NightFilter();
+        Bitmap ZenFilter();
     }
 }
